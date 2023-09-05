09204fa4d47fb5f028a2cd9ca9b91bd7621ed6e9
Andrew Galle <Andrew.Galle@LIVE.COM>
2023-09-05 19:37:37 -0400

    Add project files.

---
+++ FreePetParentWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreePetParentWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
